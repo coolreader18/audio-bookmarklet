b8f23cfdd9835c2faed614437963d563bd2ff109
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-01-09 11:36:15 -0600

    if you click it again it stops

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ function files(e) {
     var fr = new FileReader();
     fr.onload = data => {
       var res = document.querySelector("#result");
-      res.href = `javascript:new Audio("${data.currentTarget.result}").play()`
+      res.href = `javascript:(function(w,i){w[i]=w[i]||{a:new Audio("${data.currentTarget.result}"),p:!1};var o=w[i];o.p?(o.a.pause(),o.a.currentTime=0,o.p=!1):(o.a.play(),o.p=!0)})(window,"${Math.random().toString(36).substr(2,7)}")`
       res.innerHTML = file.name.split('.').slice(0, -1).join(".");
     };
     fr.readAsDataURL(file);
