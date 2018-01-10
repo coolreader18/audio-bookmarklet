07a59fdcc5dbe5b434b75d8c39e9defd80cad75c
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-01-09 19:02:08 -0600

    external mp3s work, xhrs instead of the less-supported fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 function files(e) {
   e.preventDefault();
   var dt = e.dataTransfer;
+  startPreload();
   if (dt.items) {
     for (var i=0; i < dt.items.length; i++) {
       var item = dt.items[i];
@@ -12,7 +13,7 @@ function files(e) {
     }
   } else {
     for (var i=0; i < dt.files.length; i++) {
-      dothing(dt.files[i]);
+      doFile(dt.files[i]);
     }
   }
   function doFile(file) {
@@ -23,20 +24,31 @@ function files(e) {
     fr.readAsDataURL(file);
   }
   function doURL(url) {
-    url.getAsString(str => fetch(str).then(async data => {
-      var binary = '',
-      bytes = new Uint8Array(await data.clone().arrayBuffer()),
-      blob = await data.clone().blob();
-      for (var i = 0; i < bytes.byteLength; i++) {
-          binary += String.fromCharCode(bytes[i]);
+    url.getAsString(function(str) {
+      var xhr = new XMLHttpRequest();
+      xhr.responseType = "blob";
+      xhr.open("GET", str);
+      xhr.onload = function(e) {
+        var fr = new FileReader(),
+        blob = e.currentTarget.response;
+        fr.onload = function(ab) {
+          var binary = '',
+          bytes = new Uint8Array(ab.currentTarget.result);
+          for (var i = 0; i < bytes.byteLength; i++) {
+              binary += String.fromCharCode(bytes[i]);
+          }
+          process(`data:${blob.type};base64,${window.btoa(binary)}`, xhr.responseURL.split("/").slice(-1)[0]);
+        };
+        fr.readAsArrayBuffer(blob);
       }
-      process(`data:${blob.type};base64,${window.btoa(binary)}`, data.url.split("/").slice(-1)[0]);
-    }));
+      xhr.send();
+    });
   }
   function process(data, name) {
     var res = document.querySelector("#result");
-    res.href = `javascript:(function(w,i){w[i]=w[i]||{a:new Audio("${data}"),p:!1};var o=w[i];o.p?(o.a.pause(),o.a.currentTime=0,o.p=!1):(o.a.play(),o.p=!0)})(window,"${Math.random().toString(36).substr(2,7)}")`
+    res.href = `javascript:(function(w,i){w[i]=w[i]||(function(){let a=new Audio("${data}");a.onended=function(e){u(e.currentTarget)};return a})();var a=w[i];a.paused?a.play():u(a);function u(b){b.pause();b.currentTime=0}})(window.audiobookmarklet=window.audiobookmarklet||{},"${Math.random().toString(36).substr(2,7)}")`
     res.innerHTML = name.split('.').slice(0, -1).join(".");
+    stopPreload();
   }
 }
 function dragover(e) {
@@ -51,4 +63,10 @@ function dragend(e) {
   } else {
     e.dataTransfer.clearData();
   }
+}
+function startPreload() {
+  document.querySelector("#preloader").style.display = "flex"
+}
+function stopPreload() {
+  document.querySelector("#preloader").style.display = "none";
 }
