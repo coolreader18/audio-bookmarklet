15294d5cf981ef839ddbf32c0570293d31d1be3e
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-01-09 21:47:41 -0600

    Initial Commit

---
+++ audio-bookmarklet.js
@@ -0,0 +1,25 @@
+// ==Bookmarklet==
+// @name Audio Bookmarklet Bookmarklet
+// @author coolreader18
+// ==/Bookmarklet==
+
+fetch(location.href).then(async res => {
+  var binary = '',
+  bytes = new Uint8Array(await res.clone().arrayBuffer());
+  for (var i = 0; i < bytes.byteLength; i++) {
+    binary += String.fromCharCode(bytes[i]);
+  }
+  var win = open("", "audioBookmarklet", "status=no,height=100,innerWidth=300");
+  win.document.write(
+`<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto');
+a {
+  font-family: 'Roboto', sans-serif;
+  font-color: #0288d1;
+}
+</style>
+<a href='javascript:(function(w,i){w[i]=w[i]||(function(){let a=new Audio("data:${(await res.clone().blob()).type};base64,${btoa(binary)}");a.onended=function(e){u(e.currentTarget)};return a})();var a=w[i];a.paused?a.play():u(a);function u(b){b.pause();b.currentTime=0}})(window.audiobookmarklet=window.audiobookmarklet||{},"${Math.random().toString(36).substr(2,7)}")'>${decodeURIComponent(res.url.split("/").slice(-1)[0].split('.').slice(0, -1).join("."))}</a>`);
+  win.addEventListener("blur", () => {
+    win.close();
+  })
+})
