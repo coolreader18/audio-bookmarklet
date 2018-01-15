abbadad66b930d2ff57e4725754f9a22ccf3e369
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-01-15 16:28:04 -0600

    Object.create

--- audio-bookmarklet.js
+++ audio-bookmarklet.js
@@ -18,7 +18,7 @@ a {
   font-color: #0288d1;
 }
 </style>
-<a href='javascript:(function(w,i){w[i]=w[i]||(function(){let a=new Audio("data:${(await res.clone().blob()).type};base64,${btoa(binary)}");a.onended=function(e){u(e.currentTarget)};return a})();var a=w[i];a.paused?a.play():u(a);function u(b){b.pause();b.currentTime=0}})(window.audiobookmarklet=window.audiobookmarklet||{},"${Math.random().toString(36).substr(2,7)}")'>${decodeURIComponent(res.url.split("/").slice(-1)[0].split('.').slice(0, -1).join("."))}</a>`);
+<a href='javascript:(function(w,i){w[i]=w[i]||Object.create(new Audio,{src:"data:${(await res.clone().blob()).type};base64,${btoa(binary)}",onended:function(e){u(e.currentTarget)}});var a=w[i];a.paused?a.play():u(a);function u(b){b.pause();b.currentTime=0}})(window.audiobookmarklet=window.audiobookmarklet||{},"${Math.random().toString(36).substr(2,7)}")'>${decodeURIComponent(res.url.split("/").slice(-1)[0].split('.').slice(0, -1).join("."))}</a>`);
   win.addEventListener("blur", () => {
     win.close();
   })
